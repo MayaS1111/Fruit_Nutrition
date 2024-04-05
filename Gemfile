b3d0cccdad6b5bf22408e5afb80c56e18ca95d54
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-04-05 03:22:58 +0000

    started home and nutrition page

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.2.1"
 
 gem "sinatra"
 gem "sinatra-contrib"
+gem "http"
 
 # Use Puma as the app server
 gem "puma", "~> 5.0"
--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://www.fruityvice.com/api/fruit/all"
+  raw_data =  HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  @parsed_data = JSON.parse(raw_data_string)
+
+  fruit_info = @parsed_data.fetch(1)
+  @fruit_array = []
+  count = 0
+
+  @parsed_data.each do |fruit_info|
+    name = fruit_info.fetch("name")
+    @fruit_array.push(name)
+  end
+
+  erb(:home)
+end
+
+get("/nutrition") do
+  api_url = "https://www.fruityvice.com/api/fruit/all"
+  raw_data =  HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  @parsed_data = JSON.parse(raw_data_string)
+
+  fruit_info = @parsed_data.fetch(0)
+  @fruit_names = fruit_info.fetch("name")
+
+  erb(:nutrition)
 end
